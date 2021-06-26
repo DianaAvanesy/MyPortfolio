c1cdf855d0deb925773b620da57cf8e0a1f0676a
Diana Avanesian <dianaskybin@gmail.com>
2021-06-26 18:54:34 -0400

    Initial Commit

---
+++ controllers/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* Home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* About me page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About me' });
+});
+
+/* Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'My projects' });
+});
+
+/* Contact me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact me' });
+});
+
+module.exports = router;
